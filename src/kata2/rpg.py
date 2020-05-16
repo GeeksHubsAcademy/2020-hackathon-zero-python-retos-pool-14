589fd61a9f46cac0e2bf4fe35bbcb18d6eee36f6
DavidMartos <davidign1688@hotmail.com>
2020-05-16 18:23:49 +0200

    KATA2-DavidMartos

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -4,10 +4,7 @@ import random
 import string
 
 def RandomPasswordGenerator(passLen=10):
-    #
-    #
-    
-    #
-    #
+    letters = string.ascii_lowercase
+    password = ''.join(random.choice(letters) for i in range(passLen))
 
-    return ""
+    return password
