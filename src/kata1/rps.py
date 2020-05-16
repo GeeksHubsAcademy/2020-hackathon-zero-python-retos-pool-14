465a7cd0bf3d96ff410fe542d43806ea6057b8e3
tasanast <anastasx2@hotmail.com>
2020-05-16 17:33:47 +0200

    First commit

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -1,13 +1,29 @@
 from random import randint
 
 options = ["Piedra", "Papel", "Tijeras"]
+EMPATE = "Empate!"
+GANASTE = "Ganaste!"
+PERDISTE = "Perdiste!"
+win_dict = {
+    "piedra": "tijeras",
+    "papel": "piedra",
+    "tijeras": "papel",
+    }
 
 # El resultado de salida son las siguientes String
 #'Empate!'
 #'Ganaste!'
 #'Perdiste!'
 def quienGana(player, ai):
-    return ""
+    player = player.lower()
+    ai = ai.lower()
+    if player == ai:
+        result = EMPATE
+    elif win_dict[player] == ai:
+        result = GANASTE
+    else:
+        result = PERDISTE
+    return result
 
 # Entry Point
 def Game():
@@ -16,8 +32,18 @@ def Game():
     
     #
     #
-    
+    # Get player's chose
+    answer = input("Insertar el numero del opcion, Piedra[0], Papel[1], Tijeras[2]: ")
+    answer = int(answer)
+    player = options[answer]
+
+    ai = options[randint(0,2)]
+
+    print("AI ha seleccionado " + ai)
     winner = quienGana(player, ai)
 
     print(winner)
+
+if __name__ == "__main__":
+    Game()
 
