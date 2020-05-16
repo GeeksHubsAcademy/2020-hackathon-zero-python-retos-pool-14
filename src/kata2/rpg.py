264a489ec806f64920c814b5d7ef01870a09e07b
Jan <juan.perezlosa@gmail.com>
2020-05-16 19:30:22 +0200

    Reto 2 finalizado

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
 
-import random
+import random 
 import string
 
-def RandomPasswordGenerator(passLen=10):
+def RandomPasswordGenerator(passLen):
     #
-    #
-    
-    #
-    #
+    #Genero una cadena con la suma de todos los posible valores, mayusculas, minusculas, numeros y caracteres
+    valores = string.ascii_letters + string.digits + string.punctuation
+    #print(valores)
+    respuesta = ""
+    respuesta = respuesta.join([random.choice(valores) for i in range(passLen)])
+    print(respuesta)
 
-    return ""
+    #
+    #
+    return respuesta
+
+RandomPasswordGenerator(100)
